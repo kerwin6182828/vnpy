6f47bbb75d311f5157c060708fba9d66b9f0b24b
kerwin <36542293+kerwin6182828@users.noreply.github.com>
2021-03-21 19:21:38 +0800

    Update engine.py
    little typo

--- vnpy/event/engine.py
+++ vnpy/event/engine.py
@@ -66,7 +66,7 @@ class EventEngine:
         First ditribute event to those handlers registered listening
         to this type.
 
-        Then distrubute event to those general handlers which listens
+        Then distribute event to those general handlers which listens
         to all types.
         """
         if event.type in self._handlers:
